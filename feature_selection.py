d8879e5a81e0423c22c2e1233d4210da51f618a3
Ritwik Gupta <ritwik41199@gmail.com>
2020-05-20 00:33:24 +0530

    Update feature_selection.py

--- feature_selection.py
+++ feature_selection.py
@@ -14,28 +14,20 @@ l1 = {}
 for item in dataset:
     l1[item] = dataset[item].isnull().value_counts()
 #Temprory feature variable 
-features = dataset.drop(columns = ['analysis_sample_rate','artist_7digitalid','artist_latitude','artist_longitude','song_id','track_7digitalid','track_id','transfer_note','artist_id','artist_mbid','artist_playmeid','artist_mbtags','artist_mbtags_count','audio_md5','release_7digitalid','similar_artists','title','song_hotttnesss','artist_terms','artist_terms_freq','artist_terms_weight','segments_timbre','release','artist_location','artist_name'])
+
+f1  = dataset.drop(['danceability','energy','analysis_sample_rate','artist_7digitalid','artist_latitude','artist_longitude','song_id','track_7digitalid','track_id','transfer_note','artist_id','artist_mbid','artist_playmeid','artist_mbtags','artist_mbtags_count','audio_md5','release_7digitalid','similar_artists','title','song_hotttnesss','artist_terms','artist_terms_freq','artist_terms_weight','segments_timbre','release','artist_location','artist_name'],axis=1)
+
+features = pd.read_csv('Feature_List.csv').drop('Unnamed: 0',axis=1)
+features.columns = f1.columns
 features.info()
 labels = dataset['song_hotttnesss']
 
-#from sklearn.preprocessing import StandardScaler
-#sc = StandardScaler()  
-#features = sc.fit_transform(features)
-#Label Encoding of the temprory features
-#from sklearn.preprocessing import LabelEncoder
-#le = LabelEncoder()
-#features['artist_id'] = features['artist_id'].astype(str)
-#features['artist_id'] = le.fit_transform(features['artist_id'])
-"""
-mode = []
-for i, row in features.iterrows():
-    if features['mode'][i] == 1:
-        mode.append(features['mode_confidence'][i])
-    else:
-        mode.append(-features['mode_confidence'][i])
-    print(mode[i])
-features = features.drop(['mode_confidence','mode'],axis=1)
-features['mode'] = mode
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()  
+scaled_f = sc.fit_transform(features)  
+
+features = pd.DataFrame(scaled_f,columns=features.columns)
+
 """
 from sklearn.ensemble import ExtraTreesClassifier
 import matplotlib.pyplot as plt
@@ -44,18 +36,21 @@ model.fit(features,labels)
 print(model.feature_importances_) #use inbuilt class feature_importances of tree based classifiers
 #plot graph of feature importances for better visualization
 feat_importances = pd.Series(model.feature_importances_, index=features.columns)
-feat_importances.nlargest(30).plot(kind='barh')
+feat_importances.nlargest(20).plot(kind='barh')
 plt.show()
 
-features = features.drop(columns = ['danceability','energy'],axis=1)
-features.to_csv('Features_List.csv')
 """
-mode = []
-for i, row in features.iterrows():
-    if features['mode'][i] == 1:
-        mode.append(features['mode_confidence'][i])
-    else:
-        mode.append(-features['mode_confidence'][i])
-    print(mode[i])
-features = features.drop(['mode_confidence','mode'],axis=1)
-features['mode'] = mode"""        
+
+
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import LogisticRegression
+rfe_selector = RFE(estimator=LogisticRegression(), n_features_to_select=20, step=10, verbose=5)
+rfe_selector.fit(features, labels)
+rfe_support = rfe_selector.get_support()
+rfe_feature = features.loc[:,rfe_support].columns.tolist()
+print(str(len(rfe_feature)), 'selected features')
+
+features = features.loc[:,rfe_feature]
+
+features = features.drop(columns = ['danceability','energy','bars_start'],axis=1)
+features.to_csv('Features_List.csv')
